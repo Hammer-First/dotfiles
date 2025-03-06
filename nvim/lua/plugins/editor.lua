cb9b1d1858721fe14d3f93431dd71aa7b848597c
hammer-first <hammer-first@example.com>
2025-03-06 10:11:52 +0900

    chore: チャンク範囲を示すプラグインを追加

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,8 +1,21 @@
 return {
     {
+        -- オートペア用のプラグイン
         "hrsh7th/nvim-insx",
         config = function()
                 require("insx.preset.standard").setup()
+        end,
+    },
+    {
+        -- チャンク/ブロックの範囲を示すプラグイン
+        "shellRaining/hlchunk.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("hlchunk").setup({
+                chunk = {
+                    enable = true,
+                },
+            })
         end,
     },
 }
