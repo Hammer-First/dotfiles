34552d957e206348a47056406a8d18e7f39a53a1
hammer-first <hammer-first@example.com>
2025-03-27 10:45:35 +0900

    feat: 透過とコメントの色を変更

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -3,6 +3,12 @@ return {
 	{
 		"Mofiqul/dracula.nvim",
 		config = function()
+			require("dracula").setup({
+				colors = {
+					comment = "#FFB86C",
+				},
+				transparent_bg = true,
+			})
 			vim.cmd("colorscheme dracula")
 		end,
 	},
